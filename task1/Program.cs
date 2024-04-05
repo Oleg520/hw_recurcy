d5c1c8bb9d5399d9f11e24bbdbb19abbb934c099
Oleg Golubev <traicer95@gmail.com>
2024-04-05 11:56:57 +0300

    changes in task1 for correct output

--- task1/Program.cs
+++ task1/Program.cs
@@ -8,14 +8,21 @@ int m = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Введите число N: ");
 int n = Convert.ToInt32(Console.ReadLine());
 PrintNumbersAB(m,n);
-void PrintNumbersAB(int a, int b)
+
+int PrintNumbersAB(int a, int b)
 {
-    System.Console.Write($"{a} ");
     if (a < b)
-        PrintNumbersAB(a + 1, b);
+    {
+       System.Console.Write($"{a}, ");
+        return PrintNumbersAB(a + 1, b);
+    }
     else if (a > b)
-        PrintNumbersAB(a - 1, b);
+    {
+        System.Console.Write($"{a}, ");
+        return PrintNumbersAB(a - 1, b);
+    }
     else
-        return;
+        System.Console.WriteLine(a);
+        return a;
 }
 
