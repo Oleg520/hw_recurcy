14b30474ee0e6fae8108f5a0e57d1424ee91569c
Oleg Golubev <traicer95@gmail.com>
2024-04-05 12:01:30 +0300

    changes in task1 for correct output

--- task1/Program.cs
+++ task1/Program.cs
@@ -9,20 +9,20 @@ System.Console.Write("Введите число N: ");
 int n = Convert.ToInt32(Console.ReadLine());
 PrintNumbersAB(m,n);
 
-int PrintNumbersAB(int a, int b)
+void PrintNumbersAB(int a, int b)
 {
     if (a < b)
     {
        System.Console.Write($"{a}, ");
-        return PrintNumbersAB(a + 1, b);
+        PrintNumbersAB(a + 1, b);
     }
     else if (a > b)
     {
         System.Console.Write($"{a}, ");
-        return PrintNumbersAB(a - 1, b);
+        PrintNumbersAB(a - 1, b);
     }
     else
         System.Console.WriteLine(a);
-        return a;
+        return;
 }
 
--- task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0f47274ba7ace190d63e211757e6e39815275fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5c1c8bb9d5399d9f11e24bbdbb19abbb934c099")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
